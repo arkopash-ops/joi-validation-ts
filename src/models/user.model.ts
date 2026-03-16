210d39ac3d02cd90bb48d4a857ae49dea16edc94
Aryan <ark.opash@gamil.com>
2026-03-16 12:17:46 +0530

    user.model added

---
+++ src/models/user.model.ts
@@ -0,0 +1,28 @@
+import mongoose, { Schema, Document } from "mongoose";
+import { UserRole, type User } from "../types/user.types.js";
+
+export interface UserDocument extends User, Document { }
+
+const addressSchema = new Schema({
+    street: { type: String },
+    city: { type: String },
+    zipCode: { type: String },
+}, { _id: false })
+
+const userSchema = new Schema<UserDocument>({
+    name: { type: String },
+    email: { type: String, unique: true },
+    password: { type: String },
+    age: { type: Number },
+    role: {
+        type: String,
+        enum: Object.values(UserRole),
+    },
+    phone: { type: String },
+    isActive: { type: Boolean },
+    skills: { type: [String] },
+    address: addressSchema,
+}, { timestamps: true });
+
+const User = mongoose.model<UserDocument>("User", userSchema);
+export default User;
