8b3ffb91f38ed2df761b3dde855a8b8610a94508
Aryan <ark.opash@gamil.com>
2026-03-16 12:04:16 +0530

    basic setup and mongoose connection

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async (): Promise<void> => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/zod-user");
+        await mongoose.connect("mongodb://localhost:27017/joi-user");
         console.log(`MongoDB Connected.`);
     } catch (error) {
         console.error("MongoDB connection error:", error);
